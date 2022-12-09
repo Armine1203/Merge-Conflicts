fb45250e021fe5c3412b44b82373e198e263b775
Armine Lalayan <arminelalayan2003@mail.ru>
2022-12-09 15:17:17 +0400

    merge colflict

---
+++ src/main/java/names1/Alen.java
@@ -0,0 +1,4 @@
+package main.java.names;
+
+public class Alen {
+}
---
+++ src/main/java/names1/Main.java
@@ -0,0 +1,8 @@
+package main.java.names;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+        System.out.println("new change");
+    }
+}
